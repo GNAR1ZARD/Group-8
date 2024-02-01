00bc16c1fee0f9147f07cf8f8ecd1d625707d54a
SibeW <143662887+SibeW@users.noreply.github.com>
2024-01-31 16:05:25 -0800

    Create jpacman clone
    Create jpacman clone

---
+++ jpacman/src/test/java/nl/tudelft/jpacman/level/PlayerTest.java
@@ -0,0 +1,23 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.board.Direction;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import nl.tudelft.jpacman.sprite.PacManSprites;
+public class PlayerTest {
+    /**
+     * Do we get the correct value when testing whether
+     * the player is alive?
+     */
+ //   assertThat((Player.isAlive()).isEqualTo(True));
+ //   bool alive = Player.isAlive();
+    private static final PacManSprites SPRITE_STORE = new PacManSprites();
+    private PlayerFactory Factory = new PlayerFactory(SPRITE_STORE);
+    private Player ThePlayer = Factory.createPacMan();
+
+    @Test
+    void testAlive(){
+        assertThat(ThePlayer.isAlive()).isEqualTo(true);
+    }
+}
